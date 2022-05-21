1c52410ec4c549f4e22e2a43d929763635fffb0d
rosiena <89434930+rosienaaa@users.noreply.github.com>
2022-05-21 10:02:09 +0800

    Add files via upload

--- src/components/music/MusicTab.vue
+++ src/components/music/MusicTab.vue
@@ -5,8 +5,8 @@
                 <div class="linka">
                     <router-link to="/">推荐</router-link>
                     <router-link to="/music/playlist">歌单</router-link>
-                    <router-link to="/music/rankinglist">排行榜</router-link>
-                    <router-link to="/music/singer">歌手</router-link>
+                    <router-link to="/music/rankinglist" v-show="false">排行榜</router-link>
+                    <router-link to="/music/singer" v-show="false">歌手</router-link>
                 </div>
                 
             </ul>
